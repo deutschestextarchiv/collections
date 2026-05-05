b626b1ec9b6430c07f023008ab492f130055f42d
Frank Wiegand <wiegand@bbaw.de>
2026-05-05 11:49:19 +0200

    Add publish code for LR metadata

--- schemata/generate-and-publish-datasets.sh
+++ schemata/generate-and-publish-datasets.sh
@@ -18,13 +18,27 @@ find "$TEMP_DIR" -type f -size 0 -delete
 rsync -av "$TEMP_DIR"/ "$TARGET_SRV":"$TARGET_DIR"/datasets
 ssh "$TARGET_SRV" "chmod 755 '$TARGET_DIR'/datasets"
 
+# publish LR CMDI files
+rsync -av lex-res/*.xml "$TARGET_SRV":"$TARGET_DIR"/datasets-lr
+
+# generate ZIP file for collections
 ZIPFILE=$(mktemp)
 mkdir -p "$(dirname "$ZIPFILE")/datasets"
 cp "$TEMP_DIR"/* "$(dirname "$ZIPFILE")/datasets/"
-cd "$(dirname "$ZIPFILE")" && zip -r "$ZIPFILE".zip datasets
+(cd "$(dirname "$ZIPFILE")" && zip -r "$ZIPFILE".zip datasets)
 
 # publish ZIP file
 rsync -av "$ZIPFILE".zip "$TARGET_SRV":"$TARGET_DIR"/datasets.zip
 
+# generate ZIP file for lexical resources
+ZIPFILE_LR=$(mktemp)
+mkdir -p "$(dirname "$ZIPFILE_LR")/datasets-lr"
+cp "lex-res"/*.xml "$(dirname "$ZIPFILE_LR")/datasets-lr/"
+(cd "$(dirname "$ZIPFILE_LR")" && zip -r "$ZIPFILE_LR".zip datasets-lr)
+
+# publish ZIP files
+rsync -av "$ZIPFILE".zip "$TARGET_SRV":"$TARGET_DIR"/datasets.zip
+rsync -av "$ZIPFILE_LR".zip "$TARGET_SRV":"$TARGET_DIR"/datasets-lr.zip
+
 # cleanup
-rm -rf "$TEMP_DIR" "$ZIPFILE" "$(dirname "$ZIPFILE")/datasets"
+rm -rf "$TEMP_DIR" "$ZIPFILE" "$ZIPFILE_LR" "$(dirname "$ZIPFILE")/datasets" "$(dirname "$ZIPFILE")/datasets-lr"
